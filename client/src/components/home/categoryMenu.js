667cc5934daf5cd56f0dcbfd003c06d39046e3dc
Chalukya H <62981809+Chalukya-H@users.noreply.github.com>
2020-08-29 13:58:14 +0530

    'styling'

--- client/src/components/home/categoryMenu.js
+++ client/src/components/home/categoryMenu.js
@@ -9,40 +9,41 @@ class CategoryMenu extends React.Component {
     
     render() {
         return(
-            <div className = 'collapse navbar-collapse'>
-                <ul class="navbar-nav mr-auto">                    
-                        {
-                            this.props.categories ?
-                                this.props.categories.map((category,i) =>{
-                                return (
-                                    // //className="dropdown mr-2"
-                                    <li class="nav-item dropdown" key ={i+1}>
-                                         <a class="nav-link dropdown-toggle" href="#" 
-                                            id="navbarDropdown" role="button" data-toggle="dropdown" 
-                                                aria-haspopup="true" aria-expanded="false">
-                                                           {category.mainType}
-                                                            </a>
-                                            {/* <button type="button" className="btn  dropdown-toggle" data-toggle="dropdown" 
-                                                aria-haspopup="true" aria-expanded="false">
-                                                
-                                            </button> */}
-                                            <div className="dropdown-menu">
-                                                {
-                                                    category.subType.map( (sub,i) =>{
-                                                        return  <a className="dropdown-item" href={`/products/query/${sub._id}`} key ={i+1}> 
-                                                                {sub.name} </a>
-                                                    })
-                                                } 
-                                            </div>                      
-                                    </li> )
-                                })
-                            : ''
-                        }  
-                </ul>
-                
-               
-            </div>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div className = 'collapse navbar-collapse'>
+                    <ul class="navbar-nav mr-auto">                    
+                            {
+                                this.props.categories ?
+                                    this.props.categories.reverse().map((category,i) =>{
+                                    return (
+                                      
+                                        <li class="nav-item dropdown" key ={i+1}>
+                                            <a class="nav-link dropdown-toggle" href="#" 
+                                                id="navbarDropdown" role="button" data-toggle="dropdown" 
+                                                    aria-haspopup="true" aria-expanded="false">
+                                                            {category.mainType}
+                                                                </a>
+                                                {/* <button type="button" className="btn  dropdown-toggle" data-toggle="dropdown" 
+                                                    aria-haspopup="true" aria-expanded="false">
+                                                    
+                                                </button> */}
+                                                <div className="dropdown-menu">
+                                                    {
+                                                        category.subType.map( (sub,i) =>{
+                                                            return  <a className="dropdown-item" href={`/products/query/${sub._id}`} key ={i+1}> 
+                                                                    {sub.name} </a>
+                                                        })
+                                                    } 
+                                                </div>                      
+                                        </li> )
+                                    })
+                                : ''
+                            }  
+                    </ul> 
+                </div>
+            </nav>
         )
+       
     }
 }
 
